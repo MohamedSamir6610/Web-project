158d15d3deda6094277b84c766431831b880fb40
salmakhaled7002-maker <salmakhaled7002@gmail.com>
2025-11-27 21:39:15 +0200

    1

--- menu.js
+++ menu.js
@@ -1,11 +1,11 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+
 $(document).on("click", ".add-to-cart", function () {
     const name = $(this).data("name");
     const price = Number($(this).data("price"));
 
     const existing = cart.find(i => i.name === name);
-
     if(existing) existing.qty++;
     else cart.push({ name, price, qty: 1, note: "" });
 
@@ -14,25 +14,6 @@ $(document).on("click", ".add-to-cart", function () {
 });
 
 
-function saveCart() {
-    localStorage.setItem("cart", JSON.stringify(cart));
-}
-
-renderCart();
-$("#cart-footer").append(`
-    <button class="checkout-btn">Checkout</button>
-`);
-
- // Load existing cart on page load
-$("#checkout-button").on("click", function())
- {
-    if(cart.length === 0) {
-        alert("Your cart is empty! Add some items first.");
-    } 
-    else {
-        //window.location.href = "cart web project.html"; // open checkout page
-    }
-}
 $(document).on("click", ".item img, .item h3", function () {
     const parent = $(this).closest(".item");
     const selectedItem = {
