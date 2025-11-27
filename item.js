3f6f40dc836924f7a4c3cc96e77d1dc4fe6f706e
salmakhaled7002-maker <salmakhaled7002@gmail.com>
2025-11-27 19:36:37 +0200

    1

---
+++ item.js
@@ -0,0 +1,35 @@
+let item = JSON.parse(localStorage.getItem("selectedItem"));
+let qty = 1;
+
+$("#item-img").attr("src", item.img);
+$("#item-name").text(item.name);
+$("#item-desc").text(item.desc);
+$("#item-price").text(item.price + " EGP");
+
+$("#plus").on("click", function() {
+    qty++;
+    $("#qty").text(qty);
+});
+
+$("#minus").on("click", function() {
+    if(qty > 1) qty--;
+    $("#qty").text(qty);
+});
+
+$("#addCart").on("click", function() {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    const existing = cart.find(i => i.name === item.name);
+
+    existing 
+        ? existing.qty += qty
+        : cart.push({
+            name: item.name,
+            price: item.price,
+            qty: qty,
+            note: $("#note").val()
+        });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    window.location.href = "cart.html";
+});
