732223276303b5b313eaa1ca0a839ec99f044f26
salmakhaled7002-maker <salmakhaled7002@gmail.com>
2025-11-27 20:37:16 +0200

    1

--- menu.js
+++ menu.js
@@ -1,17 +1,27 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-
 $(document).on("click", ".add-to-cart", function () {
     const name = $(this).data("name");
     const price = Number($(this).data("price"));
 
-   
     const existing = cart.find(i => i.name === name);
-    existing ? existing.qty++ : cart.push({ name, price, qty: 1 });
 
-    saveCart();
+    if(existing) existing.qty++;
+    else cart.push({ name, price, qty: 1, note: "" });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(`${name} added to cart!`);
 });
 
-function saveCart() {
-    localStorage.setItem("cart", JSON.stringify(cart));
-}
+
+$(document).on("click", ".item img, .item h3", function () {
+    const parent = $(this).closest(".item");
+    const selectedItem = {
+        img: parent.data("img"),
+        name: parent.data("name"),
+        desc: parent.data("desc"),
+        price: parent.data("price")
+    };
+    localStorage.setItem("selectedItem", JSON.stringify(selectedItem));
+    window.location.href = "item.html";
+});
